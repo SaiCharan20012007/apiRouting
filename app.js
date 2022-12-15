a65dde5f856b9b5fbe632dfcb7dc29aff93937d9
root <saicharan20012007@gmail.com>
2022-12-15 04:58:16 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+module.exports = app;
